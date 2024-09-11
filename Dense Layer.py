050a748790956b721258ef7e9e81ce058de570fc
GongJae <73451911+whdudwo0428@users.noreply.github.com>
2024-09-11 12:08:42 +0900

    Add files via upload

--- Dense Layer.py
+++ Dense Layer.py
@@ -2,26 +2,46 @@ import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
-nnfs.init()
+nnfs.init() #랜덤시드들이 고정됨 /이 기준으로만 랜덤값이 설정됨
 
 class Layer_Dense:
-    def __init__(self, n_inputs, n_neurons): #입력갯수 출력갯수
-        self.weights = np.random.uniform(0, 1, (n_inputs, n_neurons))
+    def __init__(self, n_inputs, n_neurons, initialize_method = 'random'): #입력갯수 출력갯수
+        match initialize_method:                       #인자에서  'random'을 미리 대입시켜두면 함수를 부를 때 기본값으로 들어감
+            case 'uniform'  :
+                self.weights = np.random.uniform(0, 1, (n_inputs, n_neurons))
+            case 'xavier'   :
+                self.weights =  np.random.randn(n_inputs, n_neurons) * np.sqrt(1 / n_inputs)    # Xavier 초기화
+            case 'he'       :
+                self.weights =  np.random.randn(n_inputs, n_neurons) * np.sqrt(2 / n_inputs)    # He 초기화
+            case 'gaussian' :
+                self.weights = np.random.randn(n_inputs, n_neurons) * 0.01                      # Gaussian 초기화
+            case _: #파이썬에서 _는 아무것도 입력되지 않은 상태를 의미함
+                self.weights = np.random.randn(n_inputs, n_neurons)  # 기본 랜덤 초기화
         self.biases = np.random.uniform(0, 1, (1,n_neurons))
+        #match-case문은 파이썬 3.10이상부터 사용 가능, if elif else대신 사용 가능할듯
 
     def forward(self, inputs):
         layers_outputs = np.dot(inputs, np.array(self.weights)) + self.biases
         return layers_outputs
+    def activation_Func(self, ):
+        def relu(output):
+            return np.maximum(0, layers_outputs)
+        def sigmoid(output):
+            return 1 / (1 + np.exp(-layers_outputs))
+        def tanh(output):
+            return np.tanh(layers_outputs)
 
-inputs,y = spiral_data(samples=100, classes=3)
+inputs,y = spiral_data(samples=2, classes=3)
 plt.scatter(inputs[:, 0], inputs[:, 1], c=y, cmap = 'brg')
 plt.show()
 
 DNN = Layer_Dense(2,5) #위 샘플은 2차원공간에서 정의되기 때문에 인풋을2로 설정해야함
-DNN2 = Layer_Dense(5,3)
+DNN2 = Layer_Dense(5,3) #인자에 위 가중치 초기화 방법 입력추가가능
 
 outputs = DNN.forward(inputs)  # forward 메서드의 결과를 저장
 print(outputs)  # 실제로 forward 메서드의 출력값을 출력
+#행렬을 설명하자면  [첫 데이터에 대한 출력값5개]
+#                [두번째 데이터에 대한 출력값5개] -------
 
 
-
+#2. 출력값 제한하기 / Forward의 결과값을 0 이상이면 그대로 출력 0 이하이면 0으로 출력으로 내보내기 / 활성화함수 or 조건문
